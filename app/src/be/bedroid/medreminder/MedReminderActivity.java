213dc966f809fd2e7d82a9dc9bd39fd039e2b826
SVPAH10 <SVPAH10@WP0722.ATOMIUM.local>
2012-02-18 16:38:58 +0100

    Created settings menu

--- app/src/be/bedroid/medreminder/MedReminderActivity.java
+++ app/src/be/bedroid/medreminder/MedReminderActivity.java
@@ -1,7 +1,11 @@
 package be.bedroid.medreminder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MedReminderActivity extends Activity {
 
@@ -10,6 +14,24 @@ public class MedReminderActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settingsMenu:
+			Intent intent = new Intent(this, EditPreferencesActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
